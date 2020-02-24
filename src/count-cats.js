6060c7687ea12e1ddbc4d4e029a52c6919cc4dc8
tGulevich <t.ananich@gmail.com>
2020-02-24 13:19:11 +0300

    feat: add count-cats solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let result = 0;
+  matrix.forEach(el => {
+      el.forEach(el2 => {
+        if (el2 === '^^') {
+          result += 1;
+        }
+      });
+  });
+  return result;
 };
