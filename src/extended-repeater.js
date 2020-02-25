01bd9658f63b7649c5061ab1c631b26616b622e0
tGulevich <t.ananich@gmail.com>
2020-02-25 14:01:38 +0300

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,18 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    if (typeof options.addition === "boolean") {
+        options.addition = options.addition.toString();
+    } else if (options.addition === null) {
+        options.addition = 'null';
+    }
+
+    options.separator = options.separator || '+';
+    options.addition = options.addition || '';
+
+    let ad = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition;
+    return (str + ad + options.separator).repeat(options.repeatTimes - 1) + (str + ad);
+    
+    
+    
 };
   
