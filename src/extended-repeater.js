505b98230622f9646cd7891f109f69d27e3e7cd6
tGulevich <t.ananich@gmail.com>
2020-02-25 16:10:09 +0300

    refactor: change string method

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,18 +1,12 @@
-module.exports = function repeater(str, options) {
 
-    if (typeof options.addition === "boolean") {
-        options.addition = options.addition.toString();
-    } else if (options.addition === null) {
-        options.addition = 'null';
-    }
+module.exports = function repeater(str, options) {
+    if (typeof options.addition === "boolean" || options.addition === null) {
+        options.addition = String(options.addition);
+    } 
 
     options.separator = options.separator || '+';
     options.addition = options.addition || '';
 
     let ad = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) + options.addition;
     return (str + ad + options.separator).repeat(options.repeatTimes - 1) + (str + ad);
-    
-    
-    
 };
-  
