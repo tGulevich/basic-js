3746435edebc0e874a3cb7ddc852ca5e676606a8
tGulevich <t.ananich@gmail.com>
2020-03-02 18:50:47 +0300

    add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,61 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if (!Array.isArray(arr)) {
+        throw Error; 
+    } else {
+        arr.forEach((el, i) => {
+            if (el === '--double-prev') {
+                if (arr[i-1] !== undefined) {
+                    arr[i] = arr[i-1];
+                } else {
+                    arr.splice(i, 1);
+                }
+            } else if (el === '--double-next') {
+                if (arr[i+1] !== undefined) {
+                    arr[i] = arr[i+1];
+                } else {
+                    arr.splice(i, 1);
+                }
+            }
+        });
+
+        let next = arr.indexOf('--discard-next');
+        let prev = arr.indexOf('--discard-prev');
+
+        if (next >= 0 && prev >= 0) {
+            if (next < prev ) {
+                if (arr[next+1] !== undefined) {
+                    arr.splice(next, 2);
+                } else {
+                    arr.splice(next, 1);
+                }
+            } else if (prev < next) {
+                if (arr[prev-1] !== undefined) {
+                    arr.splice(prev-1, 2);
+                } else {
+                    arr.splice(prev, 1);
+                }
+            }
+        } else if (next >= 0 && prev === -1) {
+            if (arr[next+1] !== undefined) {
+                arr.splice(next, 2);
+            } else {
+                arr.splice(next, 1);
+            }
+        } else if(prev >= 0 && next === -1) {
+            if (arr[prev-1] !== undefined) {
+                arr.splice(prev-1, 2);
+            } else {
+                arr.splice(prev, 1);
+            }
+        }
+
+        if (arr.indexOf('--discard-prev') === -1 && arr.indexOf('--discard-next') === -1) {
+            return arr;
+        } else {;
+            transform(arr);
+        }
+
+        return arr;
+    }
 };
